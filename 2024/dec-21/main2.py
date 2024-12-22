57c3e7dfa65119e4ebee272fe88c95437ab8fb05
Eugene Gubenkov <gubenkoved@gmail.com>
2024-12-22 12:10:39 +0100

    2024 dec-21 - part 2 clean checkpoint

--- 2024/dec-21/main2.py
+++ 2024/dec-21/main2.py
@@ -1,6 +1,8 @@
 import math
 import functools
 import sys
+import heapq
+from pprint import pprint
 
 sys.setrecursionlimit(10**9)
 
@@ -34,81 +36,51 @@ def position_of(keypad, char):
     return None
 
 
-# returns sequence of arrows from the start to the end coordinates on the keypad
-@functools.lru_cache()
-def all_paths(keypad, start, end):
-    sr, sc = start
-    tr, tc = end
+# returns ALL arrow paths from start to end on a given keypad
+@functools.lru_cache(maxsize=None)
+def paths2(keypad, start, end):
+    return list(paths(keypad, start, end))
+
+def paths(keypad, start, end):
+    if start == end:
+        yield ''
     r, c = start
-    cur_dist = abs(tr - sr) + abs(tc - sc)
+    tr, tc = end
+    dist = abs(tr - r) + abs(tc - c)
     neighbors = [
         (r - 1, c, '^'),
         (r + 1, c, 'v'),
         (r, c - 1, '<'),
         (r, c + 1, '>'),
     ]
-    paths = []
-    for nr, nc, nchr in neighbors:
+    for nr, nc, nd in neighbors:
         if nr < 0 or nr >= len(keypad):
             continue
         if nc < 0 or nc >= len(keypad[nr]):
             continue
-        # do not point at gaps!
+        ndist = abs(tr - nr) + abs(tc - nc)
+        if ndist >= dist:
+            continue
         if keypad[nr][nc] == '.':
             continue
-        # if we are not approaching the target, skip
-        n_dist = abs(tr - nr) + abs(tc - nc)
-        if n_dist >= cur_dist:
-            continue
-        if n_dist == 0:
-            paths.append(nchr)
-        else:
-            for inner_path in all_paths(keypad, (nr, nc), end):
-                paths.append(nchr + inner_path)
-    return paths
+        for inner in paths(keypad, (nr, nc), end):
+            yield nd + inner
 
 
-def encodings(keypad, code, pos):
+# returns ALL encoding of a given code on a given keypad
+def encodings(keypad, code, position):
     if not code:
         yield ''
         return
 
-    target_chr = code[0]
-    target_pos = position_of(keypad, target_chr)
+    next_chr = code[0]
+    next_pos = position_of(keypad, next_chr)
 
-    if pos != target_pos:
-        for arrows in all_paths(keypad, pos, target_pos):
-            for inner_result in encodings(keypad, code[1:], target_pos):
-                yield ''.join(arrows) + 'A' + inner_result
-    else:
-        for inner_result in encodings(keypad, code[1:], target_pos):
-            yield 'A' + inner_result
-
-
-def encodings_level(code, level):
-    if level == 0:
-        # final one (real keypad)
-        yield from encodings(keypad1, code, keypad1_start)
-    else:
-        for inner_code in encodings_level(code, level - 1):
-            yield from encodings(keypad2, inner_code, keypad2_start)
+    for path in paths2(keypad, position, next_pos):
+        for inner in encodings(keypad, code[1:], next_pos):
+            yield path + 'A' + inner
 
 
 def complexity(code, encoded):
     return len(encoded) * int(code[:3])
 
-
-result = 0
-for code in codes:
-    print('handling code {}'.format(code))
-    min_complexity = math.inf
-    min_encoding = None
-    for encoding in encodings_level(code, 25 + 1):
-        c = complexity(code, encoding)
-        if c < min_complexity:
-            min_complexity = c
-            min_encoding = encoding
-            print('** %s: %s (%d)' % (code, min_encoding, min_complexity))
-    print('%s: %s (%d)' % (code, min_encoding, min_complexity))
-    result += min_complexity
-print('result: %s' % result)
