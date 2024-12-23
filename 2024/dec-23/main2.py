baf5a22abb8b2b0e19f381c4fe3a2e8dc4ec963d
Eugene Gubenkov <gubenkoved@gmail.com>
2024-12-23 08:06:43 +0100

    2024 dec-23 - part 2 visualization using force-graph JS

--- 2024/dec-23/main2.py
+++ 2024/dec-23/main2.py
@@ -1,7 +1,6 @@
-from collections import defaultdict
-import pyvis
 import itertools
-
+import json
+from collections import defaultdict
 
 file = open('data.txt', 'r')
 
@@ -46,29 +45,66 @@ for node in adjacency:
 
 
 def visualize():
-    g = pyvis.network.Network(
-        # 100% does not render properly for height...
-        height='1000px', width='100%',
-        directed=True,
-    )
-    for node in adjacency:
-        g.add_node(
-            n_id=node,
-            label=node,
-            color='#0390fc' if node not in biggest else 'red',
-            font='20px Ubuntu black',
-        )
+    data = {
+        "nodes": [],
+        "links": [],
+    }
 
-    # create edges
     for node in adjacency:
+        data["nodes"].append({
+            'id': node,
+            'label': node,
+            'color': '#0390fc' if node not in biggest else 'red',
+        })
         for neighbor in adjacency[node]:
-            if node > neighbor:
+            if neighbor > node:
                 continue
-            g.add_edge(
-                source=node,
-                to=neighbor,
-            )
-    g.show('graph.html', notebook=False)
+            data["links"].append({
+                'source': node,
+                'target': neighbor,
+            })
+
+
+    with open('graph.html', 'w') as f:
+        graph_json = json.dumps(data)
+        f.write(
+"""
+<html>
+<head>
+    <script src="https://unpkg.com/force-graph"></script>
+</head>
+<body>
+    <div id="graph"></div>
+    <script>
+        const data = ##JSON##;
+    </script>
+    <script>
+        const myGraph = new ForceGraph(document.getElementById('graph'))
+            .graphData(data)
+            // .nodeCanvasObjectMode(() => 'after')
+            .nodeCanvasObject((node, ctx, globalScale) => {
+              const label = node.id;
+              const fontSize = 14 / globalScale;
+              ctx.font = `${fontSize}px Sans-Serif`;
+              const textWidth = ctx.measureText(label).width;
+              const bckgDimensions = [textWidth, fontSize].map(n => n + fontSize * 0.2); // some padding
+    
+              ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+              ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+    
+              ctx.textAlign = 'center';
+              ctx.textBaseline = 'middle';
+              ctx.fillStyle = node.color;
+              ctx.fillText(label, node.x, node.y);
+    
+              node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+            });
+    </script>
+</body>
+</html>
+""".replace('##JSON##', graph_json)
+)
+
 
 print(len(biggest))
 print(','.join(sorted(biggest)))
