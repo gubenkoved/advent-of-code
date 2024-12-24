35d798a893822ed0a5f3c066cebde184d5defc8e
Eugene Gubenkov <gubenkoved@gmail.com>
2024-12-24 16:42:55 +0100

    2024 day 24 part 2 - cleaner simulator (still too long)

--- 2024/dec-24/main2.py
+++ 2024/dec-24/main2.py
@@ -1,6 +1,8 @@
+import collections
+import functools
 import re
 import json
-
+import random
 
 file = open('data.txt', 'r')
 
@@ -23,109 +25,157 @@ while True:
 
     assert match is not None
 
-    gates.append((
+    gates.append([
         match.group(1),
         match.group(2),
         match.group(3),
-        match.group(4)
-    ))
-
-
-class CycleFound(Exception):
-    pass
+        match.group(4),
+    ])
 
 
 class Simulator:
     def __init__(self, gates):
-        self.gates = gates
-        self.gates_map = {}
-        self.value_map = {}
+        # name -> operation
+        self.gate_types = {}
+        # name -> input names
+        self.in_map = collections.defaultdict(list)
+        # name -> output names
+        self.out_map = collections.defaultdict(list)
+
+        # process gates
         for a, op, b, r in gates:
-            assert r not in self.gates_map
-            self.gates_map[r] = (a, op, b)
-        # gate -> value
-        self.cache = {}
+            assert r not in self.gate_types
+            self.gate_types[r] = op
 
-    # TODO: build dependency map using disjoint set?
-    def swap(self, idx1, idx2):
-        gate1 = gates[idx1]
-        gate2 = gates[idx2]
+            self.in_map[r].append(a)
+            self.in_map[r].append(b)
 
-        tmp = self.gates_map[gate1[-1]]
-        self.gates_map[gate1[-1]] = self.gates_map[gate2[-1]]
-        self.gates_map[gate2[-1]] = tmp
+            self.out_map[a].append(r)
+            self.out_map[b].append(r)
 
-        # TODO: invalidate only affected ones (outputs) recursively
-        #  and the outputs where invalid are input
-        # clear the cache
-        self.cache.clear()
+        # sanity checks
+        for gate in self.in_map:
+            # only two inputs are for each gate
+            assert len(self.in_map[gate]) == 2
 
-    # TODO: cache values on individual gates based on state of the inputs,
-    #   no changes in inputs -> no changes in the outputs
-    def calculate(self, reg):
-        return self.calculate_impl(reg, set())
+    def swap(self, gate1, gate2):
+        # update gate type
+        self.gate_types[gate1], self.gate_types[gate2] = self.gate_types[gate2], self.gate_types[gate1]
+        # update adjacency
+        self.in_map[gate1], self.in_map[gate2] = self.in_map[gate2], self.in_map[gate1]
+        self.out_map[gate1], self.out_map[gate2] = self.out_map[gate2], self.out_map[gate1]
 
-    def calculate_impl(self, reg, dependent: set):
-        if reg in dependent:
-            raise CycleFound()
+    def has_cycles(self):
+        visited = set()
+        stack = set()
 
-        if reg in self.value_map:
-            return self.value_map[reg]
+        # returns true if cycle is detected
+        def dfs(name: str) -> bool:
+            if name in stack:
+                return True
+            if name in visited:
+                return False
+            visited.add(name)
+            stack.add(name)
+            for in_gate in self.in_map.get(name, []):
+                if dfs(in_gate):
+                    return True
+            stack.discard(name)
+            return False
 
-        # do not recalculate the same intermediary register multiple times
-        if reg in self.cache:
-            return self.cache[reg]
+        for name in self.gate_types:
+            if dfs(name):
+                return True
 
-        a, op, b = self.gates_map[reg]
+        return False
 
-        dependent = dependent | { reg }
+    def distances_from(self, name, use_out=False):
+        queue = [(name, 0)]
+        dist_map = {}
+        visited = set()
+        while queue:
+            cur, dist = queue.pop(0)
+            if cur in visited:
+                continue
+            visited.add(cur)
+            dist_map[cur] = dist
+            adjacent = self.in_map.get(cur, [])
+            if use_out:
+                adjacent += self.out_map.get(cur, [])
+            for name in adjacent:
+                queue.append((name, dist + 1))
+        return dist_map
 
-        if op == 'OR':
-            result = self.calculate_impl(a, dependent) | self.calculate_impl(b, dependent)
-        elif op == 'XOR':
-            result = self.calculate_impl(a, dependent) ^ self.calculate_impl(b, dependent)
-        elif op == 'AND':
-            result = self.calculate_impl(a, dependent) & self.calculate_impl(b, dependent)
-        else:
-            assert False
+    def sim(self, value_map):
+        @functools.cache
+        def value_of(name):
+            if name in value_map:
+                return value_map[name]
 
-        self.cache[reg] = result
+            assert name in self.gate_types
+            op = self.gate_types[name]
+
+            if op == 'AND':
+                result = True
+                for in_gate in self.in_map[name]:
+                    result &= value_of(in_gate)
+            elif op == 'OR':
+                result = False
+                for in_gate in self.in_map[name]:
+                    result |= value_of(in_gate)
+            elif op == 'XOR':
+                result = False
+                for in_gate in self.in_map[name]:
+                    result ^= value_of(in_gate)
+
+            return result
+
+        result_map = {}
+        for gate, op in self.gate_types.items():
+            result_map[gate] = value_of(gate)
+        return result_map
+
+    def set_inout_register(self, reg_map, number, prefix):
+        for idx in range(45):
+            reg_map['%s%02d' % (prefix, idx)] = number % 2
+            number //= 2
+
+    def get_output(self, values_map):
+        result = 0
+        for idx in range(46):
+            if values_map['z%02d' % idx]:
+                result |= 1 << idx
         return result
 
-    # passes x and y to circuits and computes z
-    def sim(self, x, y):
-        for idx in range(45):
-            self.value_map['x%02d' % idx] = x % 2
-            x = x // 2
-        for idx in range(45):
-            self.value_map['y%02d' % idx] = y % 2
-            y = y // 2
-        # capture the result
-        z = 0
-        for idx in range(46):
-            zv = self.calculate('z%02d' % idx)
-            z |= zv << idx
-        return z
+    def sim2(self, x, y):
+        reg_map = {}
+        self.set_inout_register(reg_map, x, 'x')
+        self.set_inout_register(reg_map, y, 'y')
+        result_map = self.sim(reg_map)
+        return self.get_output(result_map)
 
-    def check_circuit2(self):
-        error_count = 0
-        for x_bit in range(45):
-            for y_bit in range(45):
-                x = 1 << x_bit
-                y = 1 << y_bit
-                z = self.sim(x, y)
-                good = z == x + y
-                if not good:
-                    # print('%d + %d -> %d (%s)' % (x, y, z, 'OK' if good else 'ERR'))
-                    error_count += 1
-        return error_count
+    def is_correct2(self):
+        # try setting par of bits at all possible places
+        for x_idx in range(45):
+            for y_idx in range(45):
+                x = 1 << x_idx
+                y = 1 << y_idx
+                result = self.sim2(x, y)
+                if result != x + y:
+                    print(' WA when x=%d, y=%d, got %d, expected %d' % (x, y, result, x + y))
+                    return False
+        return True
 
-    def check_circuit(self):
-        try:
-            return self.check_circuit2()
-        except CycleFound:
-            print('cycle found')
-            return float('+inf')
+    # probability based
+    def is_correct(self):
+        for _ in range(10):
+            x = random.randint(0, 2 ** 44 - 1)
+            y = random.randint(0, 2 ** 44 - 1)
+            result = self.sim2(x, y)
+            if result != x + y:
+                print(' WA when x=%d, y=%d, got %d, expected %d' % (x, y, result, x + y))
+                return False
+        return True
 
 
 def visualize():
@@ -222,60 +272,77 @@ def visualize():
             """.replace('##JSON##', graph_json)
         )
 
-
 # just look at the graph!
+# (z18, hmt)
+# (bfq, z27)
+# (z31, hkh)
+# (bng, fjp)
 # bfq,bng,fjp,hkh,hmt,z18,z27,z31
 visualize()
 
-# simulator = Simulator(gates)
-#
-# # initially broken
-# assert simulator.check_circuit() > 0
-#
-# n = len(gates)
-#
-#
-# # backtracking search to find all the 4 broken ones
-# def finder(swaps, swapped_indexes):
-#     print('checking with swaps %r' % swaps)
-#
-#     # by definition, there are  4 pairs of gates that are swapped
-#     if len(swaps) == 4:
-#         if simulator.check_circuit():
-#             print('SOLVED! %r' % swaps)
-#             return True
-#     else:
-#         # pick a pair to swap that reduces error count
-#         baseline_error_count = simulator.check_circuit()
-#         print('baseline error count: %r' % baseline_error_count)
-#
-#         for idx1 in range(n):
-#             for idx2 in range(idx1 + 1, n):
-#                 if idx1 in swapped_indexes:
-#                     continue
-#                 if idx2 in swapped_indexes:
-#                     continue
-#
-#                 print('  trying swap (%d, %d)' % (idx1, idx2))
-#                 simulator.swap(idx1, idx2)
-#                 error_count = simulator.check_circuit()
-#                 print('    new error count: %r' % error_count)
-#
-#                 # FIXME: this is a wrong assumption that we have a monotonically
-#                 #  decreasing error count
-#                 if error_count >= baseline_error_count:
-#                     # swap back
-#                     simulator.swap(idx1, idx2)
-#                     continue
-#
-#                 swaps.append((idx1, idx2))
-#                 swapped_indexes.add(idx1)
-#                 swapped_indexes.add(idx2)
-#                 finder(swaps, swapped_indexes)
-#                 swaps.pop(-1)
-#                 swapped_indexes.discard(idx1)
-#                 swapped_indexes.discard(idx2)
-#
-#                 simulator.swap(idx1, idx2)
-#
-# finder([], set())
+simulator = Simulator(gates)
+
+print('initially circuit is not good, see: ')
+assert simulator.is_correct() == False
+assert simulator.has_cycles() == False
+
+dist_map = simulator.distances_from('z45', use_out=True)
+
+# sort gates by distance from the last one (desc)
+gates = sorted(gates, key=lambda x: dist_map.get(x[-1], float('inf')), reverse=True)
+for a, op, b, r in gates:
+    suffix = ' *** ' if r.startswith('z') else ''
+    print('%3.0f  |  %3s %3s %3s -> %3s%s' % (dist_map.get(r, float('inf')), a, op, b, r, suffix))
+
+print('\n***\n')
+for idx in range(45 + 1):
+    gate = 'z%02d' % idx
+    # show up to 3 levels of the inputs
+    dist_map = simulator.distances_from(gate, use_out=True)
+    for d in range(3 + 1):
+        for a, op, b, r in sorted(gates, key=lambda g: g[1]):
+            if dist_map.get(r) != d:
+                continue
+            print('%3d  |  %3s %3s %3s -> %3s' % (dist_map[r], a, op, b, r))
+    print('')
+
+n = len(gates)
+
+# try fixing with very limited the max "distance" between error gates
+def fix(swaps, max_swaps, max_dist):
+    if len(swaps) == max_swaps:
+        print('checking for swaps: %s' % swaps)
+        if simulator.has_cycles():
+            print('  cycles detected')
+        else:
+            if simulator.is_correct():
+                print('fixed! swaps: %s' % swaps)
+                exit(0)
+    else:  # recursion step
+        start_idx = 0 if not swaps else swaps[-1][1] + 1
+        for idx1 in range(start_idx, n):
+            for idx2 in range(idx1 + 1, n):
+                if idx2 - idx1 > max_dist:
+                    continue
+                g1, g2 = gates[idx1], gates[idx2]
+                simulator.swap(g1[-1], g2[-1])
+                fix(swaps + [(idx1, idx2)], max_swaps, max_dist)
+                simulator.swap(g1[-1], g2[-1])
+
+
+# FIXME: still too long...
+print('fixing...')
+fix([], max_swaps=4, max_dist=10)
+
+# TODO: use pattern to check all summators except first two and the last one
+# XOR Z_n
+#     OR # carryover
+#         AND
+#             X_n-1
+#             Y_n-1
+#         AND
+#             OR  # prev carryover
+#             XOR
+#     XOR
+#         X_n
+#         Y_n
