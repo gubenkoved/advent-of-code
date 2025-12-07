9308eb350ddb5f9729ae1b396ab91f942e7f89db
Eugene Gubenkov <gubenkoved@gmail.com>
2025-12-07 11:28:04 +0100

    2025 dec-07 - part 2

---
+++ 2025/dec-07/part2.go
@@ -0,0 +1,61 @@
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Pos [2]int
+
+func main() {
+	data, _ := os.ReadFile("data.txt")
+	lines := strings.Split(string(data), "\n")
+
+	// count of ways to reach a given point
+	counter := map[Pos]int{}
+
+	// columns where the beam is for the next row
+	beamColums := map[int]bool{}
+	nextBeamColumns := map[int]bool{}
+
+	// simulate the beam downwards
+	rows := len(lines)
+	cols := len(lines[0])
+	for row := 0; row < rows; row++ {
+		for col := 0; col < cols; col++ {
+			pos := Pos{row, col}
+			char := lines[row][col]
+
+			if char == 'S' {
+				nextBeamColumns[col] = true
+				counter[Pos{row + 1, col}] += 1
+			}
+
+			if beamColums[col] {
+				// there is a beam from previous line here
+				if char == '^' {
+					nextBeamColumns[col-1] = true
+					nextBeamColumns[col+1] = true
+
+					counter[Pos{row + 1, col - 1}] += counter[pos]
+					counter[Pos{row + 1, col + 1}] += counter[pos]
+				} else if char == '.' {
+					counter[Pos{row + 1, col}] += counter[pos]
+					nextBeamColumns[col] = true
+				}
+			}
+		}
+		beamColums = nextBeamColumns
+		nextBeamColumns = map[int]bool{}
+	}
+
+	// sum the ways for the last row
+	sum := 0
+	for col := 0; col < cols; col++ {
+		sum += counter[Pos{rows - 1, col}]
+	}
+	fmt.Println(sum)
+}
