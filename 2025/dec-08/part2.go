17bb177496f1799edfb26cd9beb1dd9e6d5f91f0
Eugene Gubenkov <gubenkoved@gmail.com>
2025-12-08 09:31:00 +0100

    2025 dec-08 - part 2

---
+++ 2025/dec-08/part2.go
@@ -0,0 +1,119 @@
+//go:build ignore
+
+package main
+
+import (
+	"cmp"
+	"fmt"
+	"math"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type Point struct {
+	X int
+	Y int
+	Z int
+}
+
+type Line struct {
+	A      Point
+	B      Point
+	AIndex int
+	BIndex int
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func Dist(a, b Point) float64 {
+	dx := Abs(a.X - b.X)
+	dy := Abs(a.Y - b.Y)
+	dz := Abs(a.Z - b.Z)
+	return math.Sqrt(float64(dx*dx + dy*dy + dz*dz))
+}
+
+func Len(line Line) float64 {
+	return Dist(line.A, line.B)
+}
+
+func main() {
+	data, _ := os.ReadFile("data.txt")
+
+	points := []Point{}
+	for _, line := range strings.Split(string(data), "\n") {
+		components := strings.Split(line, ",")
+
+		x, _ := strconv.Atoi(components[0])
+		y, _ := strconv.Atoi(components[1])
+		z, _ := strconv.Atoi(components[2])
+
+		points = append(points, Point{
+			X: x,
+			Y: y,
+			Z: z,
+		})
+	}
+
+	lines := []Line{}
+
+	for i := 0; i < len(points); i++ {
+		for j := i + 1; j < len(points); j++ {
+			lines = append(lines, Line{
+				A:      points[i],
+				B:      points[j],
+				AIndex: i,
+				BIndex: j,
+			})
+		}
+	}
+
+	// sort by the distance
+	slices.SortFunc(lines, func(a, b Line) int {
+		return cmp.Compare(Len(a), Len(b))
+	})
+
+	// disjoint set init
+	parent := make([]int, len(points)) // point index -> parent point index
+	for idx := 0; idx < len(points); idx++ {
+		parent[idx] = idx
+	}
+
+	parentOf := func(x int) int {
+		for x != parent[x] {
+			x = parent[x]
+		}
+		return x
+	}
+
+	componentCounts := func() int {
+		counts := map[int]int{}
+		for pointIdx := 0; pointIdx < len(points); pointIdx++ {
+			parentIdx := parentOf(pointIdx)
+			counts[parentIdx] += 1
+		}
+		return len(counts)
+	}
+
+	var prevLine Line
+
+	for lineIdx := 0; componentCounts() != 1; lineIdx++ {
+		line := lines[lineIdx]
+
+		fmt.Printf("Connecting %+v\n", line)
+
+		aParent := parentOf(line.AIndex)
+		bParent := parentOf(line.BIndex)
+
+		parent[bParent] = aParent
+		prevLine = line
+	}
+
+	fmt.Println(prevLine.A.X * prevLine.B.X)
+}
